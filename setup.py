1e02e5d76014e590a13d167d52f12bcf5cb2ac1e
Mathieu Agopian <mathieu@agopian.info>
2014-12-05 11:06:04 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='test_sugardough',
+      version='0.1dev',
+      description='This is https://github.com/mozilla/test_sugardough',
+      author='Mathieu',
+      author_email='',
+      url='https://github.com/mozilla/test_sugardough')
